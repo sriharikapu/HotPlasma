5ef1862c490429ae13cf439fa56b16fd1a8b9b87
sriharikapu <sriharikapu@mail.com>
2017-08-12 10:29:05 +0800

    Create AllSensors.ino

---
+++ AllSensors.ino
@@ -0,0 +1,116 @@
+/*
+
+*/#define IRpin_PIN PIND
+#define IRpin 2
+// for MEGA use these!
+//#define IRpin_PIN PINE
+//#define IRpin 4
+ 
+// the maximum pulse we'll listen for - 65 milliseconds is a long time
+#define MAXPULSE 65000
+ 
+// what our timing resolution should be, larger is better
+// as its more 'precise' - but too large and you wont get
+// accurate timing
+#define RESOLUTION 20
+
+//TMP36 Pin Variables
+int sensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
+                        //the resolution is 10 mV / degree centigrade with a
+                        //500 mV offset to allow for negative temperatures
+ int lightPin = 0;  //define a pin for Photo resistor
+int ledPin=11; 
+/*
+ * setup() - this function runs once when you turn your Arduino on
+ * We initialize the serial connection with the computer
+ */
+void setup()
+{
+  Serial.begin(9600);  //Start the serial connection with the computer
+                       //to view the result open the serial monitor 
+                       
+                       Serial.begin(9600);
+  Serial.println("Ready to decode IR!");
+     Serial.begin(9600);  //Begin serial communcation
+    pinMode( ledPin, OUTPUT );
+}
+ uint16_t pulses[100][2]; // pair is high and low pulse
+uint8_t currentpulse = 0; // index for pulses we're storing
+void loop()                     // run over and over again
+{
+ //getting the voltage reading from the temperature sensor
+ int reading = analogRead(sensorPin);  
+ 
+ // converting that reading to voltage, for 3.3v arduino use 3.3
+ float voltage = reading * 5.0;
+ voltage /= 1024.0; 
+ 
+ // print out the voltage
+ Serial.print(voltage); Serial.println(" volts");
+ 
+ // now print out the temperature
+ float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+ Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ // now convert to Fahrenheit
+ float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+ Serial.print(temperatureF); 
+Serial.println(" degrees F");
+ 
+ delay(1000);                                     //waiting a second
+  Serial.println(analogRead(lightPin)); //Write the value of the photoresistor to the serial monitor.
+    analogWrite(ledPin, analogRead(lightPin)/4);  //send the value to the ledPin. Depending on value of resistor 
+                                                //you have  to divide the value. for example, 
+                                                //with a 10k resistor divide the value by 2, for 100k resistor divide by 4.
+   delay(10); //short delay for faster response to light.
+    uint16_t highpulse, lowpulse; // temporary storage timing
+  highpulse = lowpulse = 0; // start out with no pulse length
+  
+  
+// while (digitalRead(IRpin)) { // this is too slow!
+    while (IRpin_PIN & (1 << IRpin)) {
+     // pin is still HIGH
+ 
+     // count off another few microseconds
+     highpulse++;
+     delayMicroseconds(RESOLUTION);
+ 
+     // If the pulse is too long, we 'timed out' - either nothing
+     // was received or the code is finished, so print what
+     // we've grabbed so far, and then reset
+     if ((highpulse >= MAXPULSE) && (currentpulse != 0)) {
+       //printpulses();
+       //currentpulse=0;
+       return;
+     }
+}
+
+// we will store up to 100 pulse pairs (this is -a lot-)
+
+
+ 
+
+  // we didn't time out so lets stash the reading
+  pulses[currentpulse][0] = highpulse;
+  
+  // same as above
+  while (! (IRpin_PIN & _BV(IRpin))) {
+     // pin is still LOW
+     lowpulse++;
+     delayMicroseconds(RESOLUTION);
+     if ((lowpulse >= MAXPULSE) && (currentpulse != 0)) {
+       //printpulses();
+       currentpulse=0;
+       return;
+     }
+  }
+  pulses[currentpulse][1] = lowpulse;
+ 
+  // we read one high-low pulse successfully, continue!
+  currentpulse++;
+
+    //define a pin for LED
+
+}
+
